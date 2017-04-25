7370307c70dbc9810cef7914df46bd1e022b269e
cindy <cindialy@gmail.com>
2017-04-24 22:47:00 -0400

    ejercicio3

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+
+function convertirCelsius(){
+  var cel= document.getElementById("farenheit").value;
+  var grados= (cel * 1.8) + 32;
+  var resultado= document.getElementById("Calculo").value= grados;
+};
